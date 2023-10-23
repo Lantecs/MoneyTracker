91f19d6064ae7fdaf7534967d551079445784a79
Lanbert Ry V. Tecson <baqueranrose@gmail.com>
2023-10-23 23:40:08 +0800

    Pogress Login Page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
-    @yield('con')
+    @yield('content')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,28 @@
 @extends('layout')
+@section('title', 'Login')
+@section('content')
+
+<div class="container d-flex justify-content-center">
+    <div class="container">
+        <div class="container">
+            <h3 class="fw-bold">Welcome!</h3>
+            <p>Please enter your details:</p>
+        </div>
+        <div class="container">
+            <form>
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email:</label>
+                    <input type="email" class="form-control narrow-input" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password:</label>
+                    <input type="password" class="form-control narrow-input" id="exampleInputPassword1">
+                    <p>Forgot password</p>
+                </div>
+                <button type="submit" class="btn btn-success">Sign in</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+@endsection

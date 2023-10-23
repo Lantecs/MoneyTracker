06f7377dfc8ddaa18191fc67bd6e2f91bf02a1b3
Lanbert Ry V. Tecson <baqueranrose@gmail.com>
2023-10-24 00:51:52 +0800

    Pogress Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,27 +2,37 @@
 @section('title', 'Login')
 @section('content')
 
-<div class="container d-flex justify-content-center">
-    <div class="container">
-        <div class="container">
-            <h3 class="fw-bold">Welcome!</h3>
-            <p>Please enter your details:</p>
+
+<div class="container-fluid bg-primary">
+
+    <div class="row">
+        <div class="col">
+            <div class="container bg-light">
+                <h3 class="fw-bold">Welcome!</h3>
+                <p>Please enter your details:</p>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email:</label>
+                        <input type="email" class="form-control narrow-input" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password:</label>
+                        <input type="password" class="form-control narrow-input" id="exampleInputPassword1">
+                        <a href="#">Forgot password</a>
+                    </div>
+                    <button type="submit" class="btn btn-success">Sign in</button>
+                </form>
+            </div>
         </div>
-        <div class="container">
-            <form>
-                <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Email:</label>
-                    <input type="email" class="form-control narrow-input" id="exampleInputEmail1" aria-describedby="emailHelp">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleInputPassword1" class="form-label">Password:</label>
-                    <input type="password" class="form-control narrow-input" id="exampleInputPassword1">
-                    <p>Forgot password</p>
-                </div>
-                <button type="submit" class="btn btn-success">Sign in</button>
-            </form>
+
+        <div class="col bg-success">
+            <div class="container">
+                2 of 2
+            </div>
         </div>
     </div>
 </div>
+
+
 
 @endsection
